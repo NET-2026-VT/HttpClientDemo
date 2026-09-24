f51fe2764d9af88feecbc8ef5a9bbed102a4dd9c
Jonathan Krall <jonathan.krall@lexicon.se>
2026-09-24 11:37:33 +0200

    Registered service for HttpClient and used in HomeController. Added SimpleGet method.

--- HttpClientDemo.Client/Controllers/HomeController.cs
+++ HttpClientDemo.Client/Controllers/HomeController.cs
@@ -1,14 +1,38 @@
 using HttpClientDemo.Client.Models;
+using HttpClientDemo.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace HttpClientDemo.Client.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        private readonly HttpClient _httpClient;
+
+        public HomeController(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7045/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var res = await SimpleGet();
             return View();
+        }
+
+        private async Task<IEnumerable<StudentDto>> SimpleGet()
+        {
+            var response = await _httpClient.GetAsync("api/students");
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var students = JsonSerializer.Deserialize<IEnumerable<StudentDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return students; 
         }
 
         public IActionResult Privacy()
--- HttpClientDemo.Client/Program.cs
+++ HttpClientDemo.Client/Program.cs
@@ -9,6 +9,9 @@ namespace HttpClientDemo.Client
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+
+            builder.Services.AddHttpClient(); 
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
