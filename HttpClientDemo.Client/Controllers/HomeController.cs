f01565baa215bae88db28605ff812869b9cddd59
Jonathan Krall <jonathan.krall@lexicon.se>
2026-09-24 11:49:48 +0200

    Added GetWithRequestMessage

--- HttpClientDemo.Client/Controllers/HomeController.cs
+++ HttpClientDemo.Client/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using HttpClientDemo.Client.Models;
 using HttpClientDemo.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace HttpClientDemo.Client.Controllers
@@ -10,17 +11,38 @@ namespace HttpClientDemo.Client.Controllers
     {
 
         private readonly HttpClient _httpClient;
+        private const string json = "application/json"; 
 
         public HomeController(HttpClient httpClient)
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri("https://localhost:7045/");
+
+            //Kan rent praktiskt sätta den här om man vill att den ska sättas på alla requests.
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(json)); 
         }
 
         public async Task<IActionResult> Index()
         {
             var res = await SimpleGet();
+            var res2 = await GetWithRequestMessage(); 
             return View();
+        }
+
+        private async Task<IEnumerable<StudentDto>> GetWithRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/students");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(json));
+
+            var response = await _httpClient.SendAsync(request); 
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var students = JsonSerializer.Deserialize<IEnumerable<StudentDto>>(result, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            return students;
+
         }
 
         private async Task<IEnumerable<StudentDto>> SimpleGet()
